b86bfa483182febbe73d24381cecae5e9d89f38c
bole915226051 <915226051@qq.com>
2022-05-09 17:53:46 +0800

    -- 基础第一次上传。

---
+++ src/main/java/com/liyuanqing/base/user/ctrl/UserController.java
@@ -0,0 +1,21 @@
+package com.liyuanqing.base.user.ctrl;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/testOne")
+    public String testOne() {
+        return "第一个入口";
+    }
+
+
+    @RequestMapping("/testTwo")
+    public String testTwo() {
+        return "出现分支问题.";
+    }
+
+}
